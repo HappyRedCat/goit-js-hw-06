14c6f661d36a67f7964ab2691fae7e5fc4943e1f
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-01-13 13:41:06 +0100

    up

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,8 @@
-// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. 
-Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
-
-// Використай цей стартовий код і виконай рефакторинг.Після оголошення об'єкта ми додали виклики методів. 
-У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
-
 const customer = {
-  username: "Mango",
+  username: 'Mango',
   balance: 24000,
   discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
+  orders: ['Burger', 'Pizza', 'Salad'],
   // Change code below this line
   getBalance() {
     return this.balance;
@@ -31,6 +25,6 @@ const customer = {
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
+customer.addOrder(5000, 'Steak');
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
